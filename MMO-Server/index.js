5eb1275df0db750939d2dd15191fac2e6b4f2a90
Ilja <duntiknews@yandex.ru>
2018-04-27 17:51:57 +0100

    Adding the Attack
    Adding Attack
    Changing Readme

--- MMO-Server/index.js
+++ MMO-Server/index.js
@@ -2,7 +2,7 @@
 var io = require('socket.io')(process.env.PORT || 3000);
 var randomWords = require('random-words');
 
-console.log('Hello, server work');
+console.log('Hi, World');
 
 // count connected players
 // var countPlayers = 0;
@@ -14,111 +14,119 @@ var users = [];
 
 // pass parameter to socked.io, and callback as second parameter
 io.on('connection', function(socket) {
-  console.log('Spawn Player and Unity Connected');
-  // give the random id
-  var playerID = randomWords();
-  // save objects about player
-  // Creating object players
-  var objectPlayer = {
-    id: playerID,
-    //position: {x: 0,y: 0}
-    x: 0,
-    y: 0
-  };
+    console.log('Spawn Player and Unity Connected');
+    // give the random id
+    var playerID = randomWords();
+    // save objects about player
+    // Creating object players
+    var objectPlayer = {
+        id: playerID,
+        //position: {x: 0,y: 0}
+        x: 0,
+        y: 0
+    };
 
-  // push the playerID to array of users id's
-  //users.push(playerID);
-  //console.log('Player ID: ', playerID);
+    // push the playerID to array of users id's
+    //users.push(playerID);
+    //console.log('Player ID: ', playerID);
 
-  // Creating a key [with player ID] inside player and set it to player
-  users[playerID] = objectPlayer;
-  console.log('Player Object: ', playerID);
+    // Creating a key [with player ID] inside player and set it to player
+    users[playerID] = objectPlayer;
+    console.log('Player Object: ', playerID);
 
-  // adding locasl player to users list
-  socket.emit('ukazanie', {
-    id: playerID
-  });
-
-
-  // broadcasting "zaspawnitj" to all users
-  socket.broadcast.emit('zaspawnitj', {
-    id: playerID
-  });
-  // ask user for current possition
-  socket.broadcast.emit('onlineposition');
-  // add connected player
-  //countPlayers++;
-  for (var userID in users) {
-    if (userID == playerID)
-      continue;
-    //connect new player
-    // {id: userID}
-    socket.emit('zaspawnitj', users[userID]);
-    console.log('new players know zaspawnitj id:', userID);
-  };
-
-  // Find player in array
-  //users.forEach(function(user) {
-  // to dont make new player, check if player connected, dont connect twice
-  //  if (user == playerID)
-  //    return;
-  // connect new player
-  //  socket.emit('zaspawnitj', {
-  //    id: user
-  //  });
-  //  console.log('new players know zaspawnitj id:', user);
-  //});
-
-  //for (i = 0; i < countPlayers; i++) {
-  //  socket.emit('zaspawnitj');
-  //  console.log('new players know zaspawnitj');
-  //}
-
-  // if user disconnected
-  socket.on('disconnect', function() {
-    console.log('player disconnect');
-    // Remove the players with playerID from list
-    // No longer in array, so this wil not work
-    //users.splice(users.indexOf(playerID), 1);
-    delete users[playerID];
-
-    //countPlayers--;
-    //Seng the action to delete user from game and telling who this player are
-    socket.broadcast.emit('deleteplayer', {
-      id: playerID
+    // adding locasl player to users list
+    socket.emit('ukazanie', {
+        id: playerID
     });
-  })
 
-  socket.on('sledovanie', function(dannie) {
-    console.log("Sledovatj zapros", dannie);
-    // who is current player
-    dannie.id = playerID;
-    // let everyplayer know updated possition
-    socket.broadcast.emit('sledovanie', dannie);
-  })
 
-  socket.on('newonlinepossition', function(dannie) {
-    console.log("Online possition", dannie);
-    // who is current player
-    dannie.id = playerID;
-    // let everyplayer know updated possition
-    socket.broadcast.emit('newonlinepossition', dannie);
-  })
+    // broadcasting "zaspawnitj" to all users
+    socket.broadcast.emit('zaspawnitj', {
+        id: playerID
+    });
+    // ask user for current possition
+    socket.broadcast.emit('onlineposition');
+    // add connected player
+    //countPlayers++;
+    for (var userID in users) {
+        if (userID == playerID)
+            continue;
+        //connect new player
+        // {id: userID}
+        socket.emit('zaspawnitj', users[userID]);
+        console.log('new players know zaspawnitj id:', userID);
+    };
 
-  // if connected then lisen event "dvizhenie"
-  socket.on('dvizenie', function(dannie) {
-    // Adding the ID of player to passing data called "dannie"
-    dannie.id = playerID;
-    // Turn data in JSON to string
-    console.log('User dvizhenie', JSON.stringify(dannie));
-    // Refrence to a object players
-    //objectPlayer.position.x = dannie.x;
-    //objectPlayer.position.y = dannie.y;
-    objectPlayer.x = dannie.x;
-    objectPlayer.y = dannie.y;
-    //console.log(objectPlayer);
-    // pass data from player to all players with possition
-    socket.broadcast.emit('dvizenie', dannie);
-  })
+    // Find player in array
+    //users.forEach(function(user) {
+    // to dont make new player, check if player connected, dont connect twice
+    //  if (user == playerID)
+    //    return;
+    // connect new player
+    //  socket.emit('zaspawnitj', {
+    //    id: user
+    //  });
+    //  console.log('new players know zaspawnitj id:', user);
+    //});
+
+    //for (i = 0; i < countPlayers; i++) {
+    //  socket.emit('zaspawnitj');
+    //  console.log('new players know zaspawnitj');
+    //}
+
+    // if user disconnected
+    socket.on('disconnect', function() {
+        console.log('player disconnect');
+        // Remove the players with playerID from list
+        // No longer in array, so this wil not work
+        //users.splice(users.indexOf(playerID), 1);
+        delete users[playerID];
+
+        //countPlayers--;
+        //Seng the action to delete user from game and telling who this player are
+        socket.broadcast.emit('deleteplayer', {
+            id: playerID
+        });
+    })
+
+    socket.on('sledovanie', function(dannie) {
+        console.log("Sledovatj zapros", dannie);
+        // who is current player
+        dannie.id = playerID;
+        // let everyplayer know updated possition
+        socket.broadcast.emit('sledovanie', dannie);
+    })
+
+    socket.on('newonlinepossition', function(dannie) {
+        console.log("Online possition", dannie);
+        // who is current player
+        dannie.id = playerID;
+        // let everyplayer know updated possition
+        socket.broadcast.emit('newonlinepossition', dannie);
+    })
+
+    // if connected then lisen event "dvizhenie"
+    socket.on('dvizenie', function(dannie) {
+        // Adding the ID of player to passing data called "dannie"
+        dannie.id = playerID;
+        // Turn data in JSON to string
+        console.log('User dvizhenie', JSON.stringify(dannie));
+        // Refrence to a object players
+        //objectPlayer.position.x = dannie.x;
+        //objectPlayer.position.y = dannie.y;
+        objectPlayer.x = dannie.x;
+        objectPlayer.y = dannie.y;
+        //console.log(objectPlayer);
+        // pass data from player to all players with possition
+        socket.broadcast.emit('dvizenie', dannie);
+    })
+
+    // to share attack
+    socket.on('atakuem', function(data) {
+        //console.log("atakuem", data);
+        data.id = thisPlayerId;
+        //console.log("atakuem", data.id);
+        io.emit('atakuem', data);
+    })
 
 })
--- README.md
+++ README.md
@@ -1,6 +1,18 @@
+<p align="center"><img width=60% src="https://github.com/anfederico/Clairvoyant/blob/master/media/Clairvoyant.png"></p>
+
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+[![GitHub stars](https://img.shields.io/github/stars/duntik/MMORPG_api_unity.svg)](https://github.com/duntik/MMORPG_api_unity/stargazers)
+
+[![Twitter](https://img.shields.io/twitter/url/https/github.com/duntik/MMORPG_api_unity.svg?style=social)](https://twitter.com/intent/tweet?text=Wow:&url=https%3A%2F%2Fgithub.com%2Fduntik%2FMMORPG_api_unity)
+
+
+[![GitHub forks](https://img.shields.io/github/forks/duntik/MMORPG_api_unity.svg)](https://github.com/duntik/MMORPG_api_unity/network)
+
+
+[![GitHub issues](https://img.shields.io/github/issues/duntik/MMORPG_api_unity.svg)](https://github.com/duntik/MMORPG_api_unity/issues)
+
+[![Unity Version](https://img.shields.io/badge/Unity-2017.3.1f1-yellowgreen.svg)]
+
 # MMORPG_api_unity
 > MMORPG game based on Unity on client side and Node with Socked.IO as server side.
-
-[![NPM Version][npm-image]][npm-url]
-![npm](https://img.shields.io/npm/dw/localeval.svg)
-![node](https://img.shields.io/node/v/passport.svg)
